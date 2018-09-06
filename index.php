de9862507773cf18b29de60b88adfc00a2e1f9ab
Dmytro Kliukin <41477858+Kandzy@users.noreply.github.com>
2018-09-06 11:55:01 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
 <?php
-require "public/index.php";
+/**
+ * Created by PhpStorm.
+ * User: dkliukin
+ * Date: 7/30/18
+ * Time: 3:10 PM
+ */
+header("Access-Control-Allow-Origin: *");
+require __DIR__."/boot/app.php";
+$app->run();
